ec7eec412098acee86ffcb747dc4cbcb03507428
MaximUstianov <50552333+MaximUstianov@users.noreply.github.com>
2020-01-27 12:39:07 +0200

    update page.js

--- src/reducers/page.js
+++ src/reducers/page.js
@@ -4,7 +4,7 @@ const initialState = {
     isClicked: false,
 }
 
-export function pageReducer(state = initialState, action, props) {
+export function pageReducer(state = initialState, action) {
     switch (action.type) {
         case GET_COLOR:
             return {...state, isClicked: true};
